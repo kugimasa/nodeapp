45ed5aaf674d519a0453594b7faa9c32340d8ee7
kugi <kugimania@gmail.com>
2019-07-02 11:25:55 +0900

    [modify] response.end

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 var server = http.createServer(
     (request, response)=>{
-        response.end('Hello Node.js!');
+        response.end('<html><body><h1><p>Welcome to Node.js</p></h1></body></html>');
     }
 );
 server.listen(3000);
