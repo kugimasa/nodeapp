0b864580f62653457368ff815bde67c0ffb3283c
kugi <kugimania@gmail.com>
2019-07-09 10:00:41 +0900

    [modify] replace text

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const http = require('http');
 const fs = require('fs');
 
-var server = http.createServer(
-    (request, response)=>{
-        fs.readFile('./index.html', 'UTF-8',
-        (error, data)=>{
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data);
-            response.end();
-        });
-    }
-);
+//サーバーを立ち上げgetFromClientを実行
+var server = http.createServer(getFromClient);
+
+//ポート番号を指定
 server.listen(3000);
 console.log('Server start!');
+
+//getFromClient関数
+function getFromClient(req,res){
+    request = req;
+    response = res;
+    fs.readFile('./index.html', 'UTF-8', 
+        (error, data)=>{
+            var content = data.
+                replace(/dummy_title/g, 'タイトルです').
+                replace(/dummy_content/g, 'これがコンテンツです。');
+
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(content);
+            response.end();
+        }
+    );
+}
