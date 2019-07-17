993f82da94e9f424390262bf95f062078753e057
kugi <kugimania@gmail.com>
2019-07-17 15:33:56 +0900

    [modify] sending form

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs','utf8');
@@ -16,26 +17,14 @@ console.log('Server start!');
 
 //getFromClient関数
 function getFromClient(request,response){
-    var url_parts = url.parse(request.url);
+    var url_parts = url.parse(request.url, true);
     switch (url_parts.pathname) { 
 
         case '/':
-            var content = ejs.render(index_page, {
-                title:"Index",
-                content:"これはテンプレです。",
-            });
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(content);
-            response.end();
+            response_index(request, response);
             break;
         case '/other':
-            var content = ejs.render(other_page, { 
-                title:"Other",
-                content:"これは新しく用意したページです。",
-            });
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(content);
-            response.end();
+            response_other(request, response);
             break;
             
         case '/style.css':
@@ -51,4 +40,53 @@ function getFromClient(request,response){
     }
 
 
+}
+
+//indexのアクセス処理
+function response_index(request, response){
+    var msg = "これはIndexページです。"
+    var content = ejs.render(index_page, {
+        title:"Index",
+        content:msg,
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(content);
+    response.end();
+}
+
+//otherのアクセス処理
+function response_other(request, response){
+    var msg = "これはOtherページです。"
+    //POSTアクセス時の処理
+    if (request.method == 'POST'){
+        var body='';
+
+        //データ受信のイベント処理
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        //データ受信終了時のイベント処理
+        request.on('end',() => {
+            var post_data = qs.parse(body); //データのパース
+            msg += 'あなたは、「' + post_data.msg + '」と書きました。';
+            var content = ejs.render(other_page, {
+                title:"Other",
+                content:msg,
+            });
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(content);
+            response.end();
+        });
+    //GETアクセス時の処理
+    } else { 
+        var msg = "ページがありません。";
+        var content = ejs.render(other_page, {
+            title:"Other",
+            content:msg,
+        });
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(content);
+        response.end();
+    }
 }
