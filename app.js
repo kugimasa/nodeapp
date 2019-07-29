90a0be71988d42599dd32cd163f547420b4605e6
kugi <kugimania@gmail.com>
2019-07-29 15:02:35 +0900

    [add] Message Board

--- app.js
+++ app.js
@@ -41,21 +41,36 @@ function getFromClient(request,response){
 }
 
 //データ用変数
-var data = {
-    'Taro':'09-999-999',
-    'Hanako':'080-888-888',
-    'Sachiko':'070-777-777',
-    'Ichiro':'060-666-666'
-};
+var data = {msg:'no messgae ...'};
 
 //indexのアクセス処理
 function response_index(request, response){
-    var msg = "これはIndexページです。"
+    //POSTアクセス時の処理
+    if(request.method == 'POST'){
+        var body='';
+        //データ受信のイベント処理
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        //データ受信終了のイベント処理
+        request.on('end', () => {
+            //データのパース
+            data = qs.parse(body);
+            write_index(request, response);
+        });
+    } else {
+        write_index(request, response);
+    }
+}
+
+//indexの表示の作成
+function write_index(request, response) {
+    var msg = "伝言を表示します。";
     var content = ejs.render(index_page, {
-        title:"Index",
+        title :"Index",
         content:msg,
         data:data,
-        filename:'data_item'
     });
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(content);
